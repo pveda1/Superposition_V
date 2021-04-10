a34b8876a59622065f2582e9d28ee52311afc50e
pveda1 <parulveda1@gmail.com>
2021-04-09 18:17:05 -0700

    init

---
+++ Superposition_Project/Superposition_Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Superposition_Project
+//
+//  Created by Parul Veda on 4/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Superposition_Project/Superposition_Project/Superposition_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Superposition_ProjectApp.swift
+//  Superposition_Project
+//
+//  Created by Parul Veda on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Superposition_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
